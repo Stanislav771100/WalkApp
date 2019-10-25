6c354e57aae28a4fcc3f78461f3033ed4f3e7333
Shtanko <sshtanko@ek-mm-053.local>
2019-10-25 17:10:01 +0300

    Login Screen

---
+++ components/LoginScreen.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react';
+import { Text, View } from 'native-base';
+import { postLogin, getProjects } from '../services/requests';
+import API from '../services/requests';
+import { StyleSheet, ScrollView, Button, TextInput } from 'react-native';
+// import { connect } from 'react-redux';
+// import { addLogin, addPassword } from '../services/Root/actions/place';
+
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [],
+      issues: [],
+      user: '',
+      login: '',
+      password: ''
+    };
+  }
+  componentDidMount() {
+    this.singIn();
+  }
+  singIn = () => {
+    console.log('d');
+    API.postLogin({
+      username: this.state.login,
+      password: this.state.password
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.dir(error);
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        <View style={styles.header}>
+          <Text style={styles.HeaderText}>LOGIN</Text>
+        </View>
+        <View style={styles.content}>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Enter your login"
+            onChangeText={login => this.setState({ login })}
+            value={this.state.login}
+          />
+          <TextInput
+            style={styles.inputStyle}
+            placeholder="Enter your password"
+            onChangeText={password => this.setState({ password })}
+            value={this.state.password}
+            secureTextEntry={true}
+          />
+          <View style={styles.buttonContainerSingIn}>
+            {Platform.OS == 'ios' ? (
+              <Button onPress={this.singIn} title="Sing In" color="#FFF" />
+            ) : (
+              <Button onPress={this.singIn} title="Submit" />
+            )}
+          </View>
+          <View style={styles.buttonContainerSingUp}>
+            {Platform.OS == 'ios' ? (
+              <Button onPress={this.trackTime} title="Sing UP" color="#FFF" />
+            ) : (
+              <Button onPress={this.trackTime} title="Submit" />
+            )}
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  buttonContainerSingUp: {
+    height: 40,
+    width: '50%',
+    borderColor: '#FFF',
+    borderRadius: 5,
+    marginTop: 20,
+
+    ...Platform.select({
+      ios: {
+        backgroundColor: '#2ec79e'
+      },
+      android: {}
+    })
+  },
+  buttonContainerSingIn: {
+    height: 40,
+    width: '50%',
+    borderColor: '#FFF',
+    borderRadius: 5,
+    marginTop: 20,
+
+    ...Platform.select({
+      ios: {
+        backgroundColor: '#32c95a'
+      },
+      android: {}
+    })
+  },
+  inputStyle: {
+    height: 50,
+    width: '70%',
+    borderColor: '#DDD',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    margin: 10,
+    backgroundColor: 'white'
+  },
+  content: {
+    height: '80%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    backgroundColor: '#32c95a'
+  },
+  HeaderText: {
+    color: '#FFF',
+    fontWeight: 'normal',
+    fontSize: 30
+  }
+});
+
+// const mapStateToProps = state => {
+//   return {
+//     login: state.login.login,
+//     password: state.password.password
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     add: name => {
+//       dispatch(addLogin(name));
+//     },
+//     delete: index => {
+//       dispatch(addPassword(index));
+//     }
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(LoginScreen);
---
+++ services/requests.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'http://10.0.4.20:3006'
+export function getProjects() {
+  return axios.get('/login.json', {
+    headers: {
+      'X-Redmine-API-Key': '2fda745bb4cdd835fdf41ec1fab82a13ddc1a54c'
+    }
+  })
+}
+
+export function postLogin (data) {
+   return axios(`/login`, {
+        method: 'post',
+      })
+}
+
+class API {
+  static postLogin (data) {
+    console.log('ddf')
+    return axios(`/users`, {
+        method: 'get',
+      })
+      .catch(error => {
+        console.dir(error)
+      })
+  }
+}
+export default API
+
